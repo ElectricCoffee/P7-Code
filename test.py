508780e693e09b16ebc51d3f7beff3e991f73298
mortenm12 <mortenm12@hotmail.com>
2017-10-16 14:53:01 +0200

    adding test

---
+++ test.py
@@ -0,0 +1,8 @@
+from ../python_dbm import Context
+c = Context(["x", "y", "z"], "c") 
+a = (c.x < 10) & (c.x - c.y > 1)
+ b = (c.x < 20)
+print a <= b    # is a included in b
+print a >= b    # is b included in a
+print a.up() | b
+print a | b
