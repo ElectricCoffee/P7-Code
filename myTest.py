7955be7c4d8764c6feaebf355274d5990b9fa619
Morten <bdamen@hotmail.com>
2017-10-18 11:31:50 +0200

    test af updateValue()

--- myTest.py
+++ myTest.py
@@ -1,6 +1,5 @@
 import sys
 sys.path.insert(0, '../')
 from python_dbm import Context
-c = Context(["x", "y"], "d")
-a = (c.x < 20)
-print a.upInPlace()
+c = Context(["x", "y", "z"], "d")
+a = (c.x == 1).updateValue(c.x, 2)
