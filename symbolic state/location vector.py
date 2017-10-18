85733af8cded9778a7d61a617be334183ddfa8ad
luraman <ponifi3d@gmail.com>
2017-10-18 13:44:10 +0200

    Inherit from list

--- symbolic state/location vector.py
+++ symbolic state/location vector.py
@@ -1,15 +1,6 @@
-class LocationVector:
-    def __init__(self, *locations):
-        self.locations = locations
-
-    def __eq__(self, other):
-            return self.locations == other.locations
-
-    def __ne__(self, other):
-        return not self == other
-
+class LocationVector(list):
     def __le__(self, other):
-            for location, otherlocation in self.locations, other.locations:
+            for location, otherlocation in self, other:
                 if otherlocation != '*' and location != otherlocation:
                     return False
             return True
