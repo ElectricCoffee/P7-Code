621f35d57445e329b66eaa063768df476e2157ca
Morten <bdamen@hotmail.com>
2017-10-18 11:38:44 +0200

    flere små test cases

--- myTest.py
+++ myTest.py
@@ -2,7 +2,7 @@ import sys
 sys.path.insert(0, '../')
 from python_dbm import Context
 c = Context(["x", "y", "z"], "d")
-a = (c.x == 1).updateValue(c.x, 2)
+a = (c.x == 1).updateValue(c.x)
 d = (c.x == 1).resetValue(c.x, 0)
 print a
 print b
