172f0f923055326443c652ff2b082d78378320f4
luraman <ponifi3d@gmail.com>
2017-10-19 11:14:39 +0200

    Peppede den op

--- tioa/tioa.py
+++ tioa/tioa.py
@@ -1,27 +1,25 @@
 class TIOA:
-
-    def is_valid_edge(self, edge):
-        return \
-            edge.initial_location in self.locations \
-            and (edge.action in self.actions_output or edge.action in self.actions_input) \
-            and edge.reset <= self.clocks \ # the set of clocks there will be reset, needs to be in the set of clocks
-            and edge.target_location in self.locations
-
     def __init__(self, locations, initial_location, clocks, edges, actions_input, actions_output, invariants):
         self.locations = locations
         self.initial_location = initial_location
-        self.clocks = clocks # clocks is a set of clocks
+        self.clocks = clocks  # clocks is a set of clocks
         self.edges = edges
         self.actions_input = actions_input
         self.actions_output = actions_output
         self.invariants = invariants
 
+    def is_valid_edge(self, edge):
+        return \
+            edge.initial_location in self.locations \
+            and (edge.action in self.actions_output or edge.action in self.actions_input) \
+            and edge.reset <= self.clocks \
+            and edge.target_location in self.locations
+
 
 class Edge:
-
     def __init__(self, initial_location, action, guard, reset, target_location):
-        self.initial_location = initial_location # is the s in s -> s'
+        self.initial_location = initial_location  # is the s in s -> s'
         self.action = action
         self.guard = guard
-        self.reset = reset # reset is a set of clocks that will be reset over the edge
-        self.target_location = target_location # is the s' in s -> s'
+        self.reset = reset  # reset is a set of clocks that will be reset over the edge
+        self.target_location = target_location  # is the s' in s -> s'
