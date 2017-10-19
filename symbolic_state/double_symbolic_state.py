3d1176592f5c9dd753184ddafbc377016784ae4b
Morten <bdamen@hotmail.com>
2017-10-19 11:38:01 +0200

    double symbolic state fil tilføjet

---
+++ symbolic_state/double_symbolic_state.py
@@ -0,0 +1,9 @@
+from location_vector import *
+
+
+class DoubleSymbolicState:
+
+    def __init__(self, location_vector, zone):
+        self.location_vector = location_vector
+        self.zone = zone
+        
