ec60b5e9537ad7874b337115074071ab901263ab
Morten <bdamen@hotmail.com>
2017-10-23 13:01:11 +0200

    ændring af import, linje 3

--- test/myTest.py
+++ test/myTest.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.insert(0, '../')
-from python_dbm import Context
+from udbm import Context
 c = Context(["x", "y", "z"], "d")
 a = (c.x == 1).updateValue(c.x, 2)
 d = (c.x == 1).resetValue(c.x)
--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.insert(0, '../')
-from python_dbm import Context
+from udbm import Context
 c = Context(["x", "y", "z"], "d") 
 a = (c.x < 10) & (c.x - c.y > 1)
 b = (c.x < 20)
