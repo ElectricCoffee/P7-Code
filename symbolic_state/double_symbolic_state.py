8aa1d8c84fb02ddbdb15a59c6c07b9df485cfb40
Morten <bdamen@hotmail.com>
2017-10-26 08:42:54 +0200

    sikring af mit code

--- symbolic_state/double_symbolic_state.py
+++ symbolic_state/double_symbolic_state.py
@@ -6,3 +6,7 @@ class DoubleSymbolicState:
     def __init__(self, location_vector, zone):
         self.location_vector = location_vector
         self.zone = zone
+
+    @staticmethod
+    def k_equivalence(k, zone, other_zone):
+        pass
