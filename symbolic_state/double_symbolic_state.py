2afd61cdd306d1c668deeb157ccd4b48e27c9694
Morten <bdamen@hotmail.com>
2017-10-26 13:52:18 +0200

    små ændringer til k-æk

--- symbolic_state/double_symbolic_state.py
+++ symbolic_state/double_symbolic_state.py
@@ -12,8 +12,8 @@ class DoubleSymbolicState:
     def k_equivalence(k, u1, u2):
         """k is a set of clocks, and
         u1 and u2 is federations and returns if they are equal in the dimensions in k"""
-        for x in k:
-            if not (u1.context.hasClockByName(x) and u2.context.hasClockByName(x)):
+        for clock in k:
+            if not (u1.context.hasClockByName(clock) and u2.context.hasClockByName(clock)):
                 return False
 
         # finding the different clocks in u1 and u2
@@ -21,20 +21,20 @@ class DoubleSymbolicState:
         u2_minus_k = diff(u2, k)
 
         # freeing the unshared clocks in u1 and k
-        for c in u1_minus_k:
-            u1.freeClock(c)
+        for clock in u1_minus_k:
+            u1.freeClock(clock)
         # freeing the unshared clocks in u2 and k
-        for c in u2_minus_k:
-            u2.freeClock(c)
+        for clock in u2_minus_k:
+            u2.freeClock(clock)
 
         # returning the comparison of the modified u1 and u2
         return u1 == u2
 
 
 def diff(u1, k):
-    """u1 and u2 is a federation, and returns the set diffrences between u1 and u2's clocks"""
+    """u1 and u2 is a federation, and returns the set differences between u1 and u2's clocks"""
     result = []
-    for c in k:
-        if not (u1.context.hasClockByName(c)):
-            result.append(c)
+    for clock in k:
+        if not (u1.context.hasClockByName(clock)):
+            result.append(clock)
     return result
