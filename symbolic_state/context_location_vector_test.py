3e5b0d8acf13f593720f51995e1314bf8053720f
luraman <ponifi3d@gmail.com>
2017-10-27 13:23:56 +0200

    Add comments Move cases Change constructors

--- symbolic_state/context_location_vector_test.py
+++ symbolic_state/context_location_vector_test.py
@@ -7,21 +7,27 @@ from tioa.automata_context import AutomataContext
 class ContextLocationVectorTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
+        #  Automata -- Only locations and initial location are defined.
         cls.automaton0 = TIOA(['x', 'y'], 'x', None, None, None, None, None)
         cls.automaton1 = TIOA(['a', 'b'], 'a', None, None, None, None, None)
         cls.automaton2 = TIOA(['f', 'g'], 'f', None, None, None, None, None)
 
+        #  Context for the context location vectors -- Includes all the automata
         context = AutomataContext([cls.automaton0, cls.automaton1, cls.automaton2])
 
-        cls.lv0 = ContextLocationVector(context, ['x', 'a', 'g'])
-        cls.lv1 = ContextLocationVector(context, ['x', 'b', 'g'])
+        #  Context location vectors created with context
+        cls.lv0 = context.ContextLocationVector(['x', 'a', 'g'])
+        cls.lv1 = context.ContextLocationVector(['x', 'b', 'g'])
 
     def test_m_equivalence(self):
+        #  Tests m_equivalence between lv0 and lv1, with all legal permutations of m.
+        #  Asserts true iff automaton1 is not in m
         self.assertTrue(self.lv0.m_equivalence(self.lv1, {self.automaton0}))
-        self.assertFalse(self.lv0.m_equivalence(self.lv1, {self.automaton1}))
         self.assertTrue(self.lv0.m_equivalence(self.lv1, {self.automaton2}))
-        self.assertFalse(self.lv0.m_equivalence(self.lv1, {self.automaton0, self.automaton1}))
         self.assertTrue(self.lv0.m_equivalence(self.lv1, {self.automaton0, self.automaton2}))
+
+        self.assertFalse(self.lv0.m_equivalence(self.lv1, {self.automaton1}))
+        self.assertFalse(self.lv0.m_equivalence(self.lv1, {self.automaton0, self.automaton1}))
         self.assertFalse(self.lv0.m_equivalence(self.lv1, {self.automaton1, self.automaton2}))
         self.assertFalse(self.lv0.m_equivalence(self.lv1, {self.automaton0, self.automaton1, self.automaton2}))
 
