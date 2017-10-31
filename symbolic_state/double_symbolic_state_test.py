d50de654e433d877b333ec411bd06e077e904add
Morten Meyer Rasmussen <mortenm12@hotmail.com>
2017-10-31 10:39:22 +0100

    adding comment to test

--- symbolic_state/double_symbolic_state_test.py
+++ symbolic_state/double_symbolic_state_test.py
@@ -64,7 +64,7 @@ class double_symbolic_state_test(unittest.TestCase):
         self.assertTrue(self.dss1.k_equivalence(self.dss1, ['x', 'y', 'z']))
 
     def testmk_equivalence(self):
-        self.assertTrue(self.dss1.mk_equivalence(self.dss2, ['x', 'y'], {self.automaton0}))
+        self.assertTrue(self.dss1.mk_equivalence(self.dss2, ['x', 'y'], {self.automaton0}))  # should return true for both
         self.assertTrue(self.dss1.mk_equivalence(self.dss2, ['x', 'y'], {self.automaton2}))
 
         self.assertFalse(self.dss1.mk_equivalence(self.dss2, ['x', 'y', 'z'], {self.automaton0}))  # k-equavalence should return false
