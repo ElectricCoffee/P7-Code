00dbc85222719656432c10b933cc6f1745b4168e
Morten Meyer Rasmussen <mortenm12@hotmail.com>
2017-11-22 11:31:14 +0100

    Adding the cbr algorithm from page 30 in Ulriks master thesis

--- cbr/cbr.py
+++ cbr/cbr.py
@@ -1,53 +1,34 @@
-#checks if it is possible to get from s0 to goal, a forward approch
-def forwardDFS(st, s0, goal):
-    if s0 == goal:
-        return True
-    elif not canTakeAnAction(st, s0):
-        return False
-    for x in allActions(st, s0):
-        if forwardDFS(st, s0, x):
-            return True
+from symbolic_state.double_symbolic_state import *
+from dbm.udbm import *
+
+
+def cbr(dss_init, dss_goal, m, k):
+    """Determind if i t is possible to go from goal to init
+
+    Keyword Arguments:
+    dss_init -- the initial Double Symbolic State
+    dss_goal -- the goal Double Symbolic State
+    m -- An iterable of machines
+    k -- An iterable of clocks
+    """
+    wait = [dss_goal]
+    m_new = []
+    k_new = []
+    for automat in m:
+        m_new.append(automat)
+        for clock in k:
+            k_new.append(clock)
+            passed = []
+            for symbolicstate in wait:
+                if symbolicstate == dss_init:
+                    return True
+                else:
+                    for symbolicstate_new in passed:
+                        if not symbolicstate <= symbolicstate_new:
+                            passed.append(symbolicstate)
+                            next = symbolicstate.mk_predecessors(m_new, k_new)
+                            for j in next:
+                                wait.append(j)
+            for x in passed:
+                wait.append(x)
     return False
-
-
-#checks if it is possible to take an action from s
-def canTakeAnAction(st, s):
-   for x in range(0, len(st[s])):
-        if st[s][x] is not None:
-            return True
-   return False
-
-
-#returns a list of every state s can go to
-def allActions(st, s):
-    act = []
-    for x in range(0, len(st[s])):
-        if st[s][x] is not None:
-            act.append(x)
-    return act
-
-
-def backwardDFS(st, s0, goal):
-    if s0 == goal:
-        return True
-    elif not thereExistAnAction(st, goal):
-        return False
-    for x in allPreState(st, goal):
-        if backwardDFS(st, s0, x):
-            return True
-    return False
-
-
-#checks if there exist an action to s in st
-def thereExistAnAction(st, s):
-    for x in range(0,len(st)):
-        if st[x][s] is not None:
-            return True
-    return False
-
-def allPreState(st, s):
-    preState = []
-    for x in range(0,len(st)):
-        if st[x][s] is not None:
-            preState.append(x)
-    return preState
