0c997d5188ad793b13d1dd264a840e9eebff4ff1
Morten Meyer Rasmussen <mortenm12@hotmail.com>
2017-11-28 11:35:36 +0100

    chaning test to give false

--- cbr/cbr_test.py
+++ cbr/cbr_test.py
@@ -18,7 +18,7 @@ class double_symbolic_state_test(unittest.TestCase):
         cls.t2 = TIOA(["d", "e", "f"], "d", ['x', 'y'],
                       [Edge("d", "g", Guard(cls.c), set(), "e"), cls.e2], {"h"}, {"g"}, {})
         cls.t3 = TIOA(["l", "m", "n"], "l", ['x', 'y'],
-                      [Edge("l", "r", Guard(cls.c), set(), "m"), Edge("m", "t", Guard(cls.c), set(), "n")], {"r"}, {"t"}, {})
+                      [Edge("l", "h", Guard(cls.c), set(), "m"), Edge("m", "g", Guard(cls.c), set(), "n")], {"h"}, {"g"}, {})
 
         cls.t4 = TIOA(["a", "b", "c"], "a", ['x', 'y'],
                       [Edge("a", "g", Guard(cls.c, (cls.c['x'], 2, '>')), set(), "b"), Edge("b", "h", Guard(cls.c), set(), "c")], {"g"}, {"h"}, {})
@@ -54,10 +54,10 @@ class double_symbolic_state_test(unittest.TestCase):
         self.dss2 = DoubleSymbolicState(self.autocon12.ContextLocationVector(["c", "f"]), self.c.getTautologyFederation())
         self.assertTrue(cbr(self.dss1, self.dss2, [self.t1, self.t2], self.clocks))
 
-    def test_cbr_true_wu_zone(self):
+    def test_cbr_false_wu_zone(self):
         self.dss1 = DoubleSymbolicState(self.autocon13.ContextLocationVector(["a", "l"]), self.c.getTautologyFederation())
         self.dss2 = DoubleSymbolicState(self.autocon13.ContextLocationVector(["c", "n"]), self.c.getTautologyFederation())
-        self.assertTrue(cbr(self.dss1, self.dss2, [self.t1, self.t3], self.clocks))
+        self.assertFalse(cbr(self.dss1, self.dss2, [self.t1, self.t3], self.clocks))
 
     def test_cbr_true_w_zone(self):
         self.dss1 = DoubleSymbolicState(self.autocon45.ContextLocationVector(["a", "d"]), self.c.getTautologyFederation())
