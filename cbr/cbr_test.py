83b878a663db240903217aaed952cace57f35cc4
Morten Meyer Rasmussen <mortenm12@hotmail.com>
2017-11-29 13:04:37 +0100

    Adding test cases for invariants, and changing list of clock names to set of clock

--- cbr/cbr_test.py
+++ cbr/cbr_test.py
@@ -13,24 +13,39 @@ class double_symbolic_state_test(unittest.TestCase):
         cls.c = Context(['x', 'y'], 'c')
         cls.e1 = Edge("b", "h", Guard(cls.c), set(), "c")
         cls.e2 = Edge("e", "h", Guard(cls.c), set(), "f")
-        cls.t1 = TIOA(["a", "b", "c"], "a", ['x', 'y'],
+        cls.t1 = TIOA(["a", "b", "c"], "a", set(cls.c.clocks),
                       [Edge("a", "g", Guard(cls.c), set(), "b"), cls.e1], {"g"}, {"h"}, {})
-        cls.t2 = TIOA(["d", "e", "f"], "d", ['x', 'y'],
+        cls.t2 = TIOA(["d", "e", "f"], "d", set(cls.c.clocks),
                       [Edge("d", "g", Guard(cls.c), set(), "e"), cls.e2], {"h"}, {"g"}, {})
-        cls.t3 = TIOA(["l", "m", "n"], "l", ['x', 'y'],
+        cls.t3 = TIOA(["l", "m", "n"], "l", set(cls.c.clocks),
                       [Edge("l", "h", Guard(cls.c), set(), "m"), Edge("m", "g", Guard(cls.c), set(), "n")], {"h"}, {"g"}, {})
 
-        cls.t4 = TIOA(["a", "b", "c"], "a", ['x', 'y'],
-                      [Edge("a", "g", Guard(cls.c, (cls.c['x'], 2, '>')), set(), "b"), Edge("b", "h", Guard(cls.c), set(), "c")], {"g"}, {"h"}, {})
-        cls.t5 = TIOA(["d", "e", "f"], "d", ['x', 'y'],
-                      [Edge("d", "g", Guard(cls.c, (cls.c['x'], 2, '>')), set(), "e"), Edge("e", "h", Guard(cls.c), set(), "f")], {"h"}, {"g"}, {})
-        cls.t6 = TIOA(["l", "m", "n"], "l", ['x', 'y'],
-                      [Edge("l", "g", Guard(cls.c, (cls.c['x'], 2, '<')), set(), "m"), Edge("m", "h", Guard(cls.c), set(), "n")], {"h"}, {"g"}, {})
+        cls.t4 = TIOA(["a", "b", "c"], "a", set(cls.c.clocks),
+                      [Edge("a", "g", Guard(cls.c, (cls.c['x'], 2, '>')), set(), "b"),
+                       Edge("b", "h", Guard(cls.c), set(), "c")], {"g"}, {"h"}, {})
+        cls.t5 = TIOA(["d", "e", "f"], "d", set(cls.c.clocks),
+                      [Edge("d", "g", Guard(cls.c, (cls.c['x'], 2, '>')), set(), "e"),
+                       Edge("e", "h", Guard(cls.c), set(), "f")], {"h"}, {"g"}, {})
+        cls.t6 = TIOA(["l", "m", "n"], "l", set(cls.c.clocks),
+                      [Edge("l", "g", Guard(cls.c, (cls.c['x'], 2, '<')), set(), "m"), #der er en fejl, i og med at clocks med samme navn på forskelige ure reelt set er to forskellige
+                       Edge("m", "h", Guard(cls.c), set(), "n")], {"h"}, {"g"}, {})
+
+        cls.t7 = TIOA(["a", "b", "c"], "a", set(cls.c.clocks),
+                      [Edge("a", "g", Guard(cls.c), set(), "b"),
+                       Edge("b", "h", Guard(cls.c), set(), "c")], {"g"}, {"h"}, {})
+        cls.t8 = TIOA(["d", "e", "f"], "d", set(cls.c.clocks),
+                      [Edge("d", "g", Guard(cls.c), set(), "e"),
+                       Edge("e", "h", Guard(cls.c), set(), "f")], {"h"}, {"g"}, {"e":(cls.c.x == 1)})
+        cls.t9 = TIOA(["l", "m", "n"], "l", set(cls.c.clocks),
+                      [Edge("l", "g", Guard(cls.c), set("x"), "m"),
+                       Edge("m", "h", Guard(cls.c), set(), "n")], {"h"}, {"g"}, {"m":(cls.c.x == 1)})
 
         cls.autocon12 = AutomataContext([cls.t1, cls.t2])
         cls.autocon13 = AutomataContext([cls.t1, cls.t3])
         cls.autocon45 = AutomataContext([cls.t4, cls.t5])
         cls.autocon46 = AutomataContext([cls.t4, cls.t6])
+        cls.autocon78 = AutomataContext([cls.t7, cls.t8])
+        cls.autocon79 = AutomataContext([cls.t7, cls.t9])
 
 
         cls.clocks = set()
@@ -68,6 +83,20 @@ class double_symbolic_state_test(unittest.TestCase):
         self.dss1 = DoubleSymbolicState(self.autocon46.ContextLocationVector(["a", "l"]), self.c.getTautologyFederation())
         self.dss2 = DoubleSymbolicState(self.autocon46.ContextLocationVector(["c", "n"]), self.c.getTautologyFederation())
         self.assertFalse(cbr(self.dss1, self.dss2, [self.t4, self.t6], self.clocks))
+
+    def test_cbr_true_w_invariants(self):
+        self.dss1 = DoubleSymbolicState(self.autocon78.ContextLocationVector(["a", "d"]),
+                                        self.c.getTautologyFederation())
+        self.dss2 = DoubleSymbolicState(self.autocon78.ContextLocationVector(["c", "f"]),
+                                        self.c.getTautologyFederation())
+        self.assertTrue(cbr(self.dss1, self.dss2, [self.t7, self.t8], self.clocks))
+
+    def test_cbr_false_w_invariants(self):
+        self.dss1 = DoubleSymbolicState(self.autocon79.ContextLocationVector(["a", "l"]),
+                                        self.c.getTautologyFederation())
+        self.dss2 = DoubleSymbolicState(self.autocon79.ContextLocationVector(["c", "n"]),
+                                        self.c.getTautologyFederation())
+        self.assertFalse(cbr(self.dss1, self.dss2, [self.t7, self.t9], self.clocks))
 
 if __name__ == '__main__':
             unittest.main()
